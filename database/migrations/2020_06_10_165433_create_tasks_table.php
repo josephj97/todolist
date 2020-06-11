8a0c3dee4aa718dfdb1af35fe823369a24591b79
Joseph Saad <josephatef.j97@gmail.com>
2020-06-11 23:09:18 +0200

    Finished the Laravel+MySQL part

---
+++ database/migrations/2020_06_10_165433_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string("Title",20);
+            $table->string("Description",100);
+            $table->boolean("Done");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
