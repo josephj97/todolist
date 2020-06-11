e59cccd33d115f97574eab9be1390f4e6b3c2130
Joseph Saad <josephatef.j97@gmail.com>
2020-06-11 23:37:35 +0200

    fixed the api routes

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('tasks','TaskController@index');
-Route::get('tasks/{id}','TaskController@show');
-Route::post('tasks','TaskController@store');
-Route::put('tasks/{id}','TaskController@update');
-Route::delete('tasks/{id}','TaskController@delete');
+Route::get('task','TaskController@index');
+Route::get('task/{id}','TaskController@show');
+Route::post('task','TaskController@store');
+Route::put('task/{id}','TaskController@update');
+Route::delete('task/{id}','TaskController@delete');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
